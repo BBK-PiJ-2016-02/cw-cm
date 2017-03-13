4ce42ad378515f4ce02f0ee6c672c869888a0545
Alun Davey <hey@4lun.io>
2017-03-13 20:05:12 +0000

    Minimal Contact implementation that compiles

---
+++ src/main/java/ContactImpl.java
@@ -0,0 +1,22 @@
+package impl;
+
+import spec.Contact;
+
+public class ContactImpl implements Contact {
+
+  public int getId() {
+    return 1;
+  }
+
+  public String getName() {
+    return null;
+  }
+
+  public String getNotes() {
+    return null;
+  }
+
+  public void addNotes(String note) {
+
+  }
+}
