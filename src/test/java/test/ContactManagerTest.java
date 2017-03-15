44a10b892d6f7e5d6ba033c4974d360093bd0161
Alun Davey <hey@4lun.io>
2017-03-15 17:13:10 +0000

    ContactManager test (failing)

--- src/test/java/test/ContactManagerTest.java
+++ src/test/java/test/ContactManagerTest.java
@@ -1,24 +1,22 @@
 package test;
 
-import impl.*;
-import spec.*;
+import static org.junit.Assert.assertEquals;
+
+import impl.ContactManagerImpl;
 
 import java.util.Set;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class ContactManagerTest {
 
-    @Test
-    public void testCreatingAndRetrievingSingleContact() {
-        ContactManager contactManager = new ContactManagerImpl();
-        String contactName = "John";
-        int contactId = contactManager.addNewContact(contactName, "A note about " + contactName);
+  @Test
+  public void testAddingContact() {
+    ContactManagerImpl contactManager = new ContactManagerImpl();
+    assertEquals(0, contactManager.getAllContacts().size());
 
-        Set<Contact> contactById = contactManager.getContacts(contactId);
-        Set<Contact> contactByName = contactManager.getContacts("John");
+    int contactId = contactManager.addNewContact("John", "A note about John");
 
-        assertEquals(contactName, ((Contact) contactById.toArray()[0]).getName());
-        assertEquals(contactName, ((Contact) contactByName.toArray()[0]).getName());
-    }
+    assertEquals(1, contactManager.getAllContacts().size());
+    assertEquals(1, contactId);
+  }
 }
