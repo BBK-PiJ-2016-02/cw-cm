5688e7c9d05de2740351aff05807572ae5de6042
Alun Davey <hey@4lun.io>
2017-03-16 18:27:17 +0000

    Manager tests now generate multiple contacts for testing IDs

--- src/test/java/test/ContactManagerTest.java
+++ src/test/java/test/ContactManagerTest.java
@@ -3,43 +3,67 @@ package test;
 import static org.junit.Assert.assertEquals;
 
 import impl.ContactManagerImpl;
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Set;
 import org.junit.Test;
+import spec.Contact;
 import spec.ContactManager;
 
 public class ContactManagerTest {
 
   @Test
-  public void testAddingContact() {
+  public void testAddingSingleContact() {
     ContactManager contactManager = new ContactManagerImpl();
-    assertEquals(0, contactManager.getContacts("").size());
+    generateTestContacts(contactManager);
+    int initContactCount = contactManager.getContacts("").size();
 
     int contactId1 = contactManager.addNewContact("John", "A note about John");
 
-    assertEquals(1, contactManager.getContacts("").size());
-    assertEquals(1, contactId1);
+    assertEquals(initContactCount + 1, contactManager.getContacts("").size());
+    assertEquals(initContactCount + 1, contactId1);
 
     int contactId2 = contactManager.addNewContact("Sarah", "A note about Sarah");
 
-    assertEquals(2, contactManager.getContacts("").size());
-    assertEquals(2, contactId2);
+    assertEquals(initContactCount + 2, contactManager.getContacts("").size());
+    assertEquals(initContactCount + 2, contactId2);
   }
 
   @Test
-  public void testRetrievingContactById() {
-    ContactManagerImpl contactManager = new ContactManagerImpl();
-    int contactId = contactManager.addNewContact("John", "A note about John");
+  public void testRetrievingSingleContactsById() {
+    ContactManager contactManager = new ContactManagerImpl();
+    generateTestContacts(contactManager);
 
-    Set<Contact> contactById = contactManager.getContacts(contactId);
-    assertEquals("John", ((Contact) contactById.toArray()[0]).getName());
+    int contactId = contactManager.addNewContact("John", "A note about John");
+    Set<Contact> contactsById = contactManager.getContacts(contactId);
+    Contact contact = (Contact) Array.get(contactsById.toArray(), 0);
+
+    assertEquals("John", contact.getName());
   }
 
   @Test
-  public void testRetrievingContactByName() {
-    ContactManagerImpl contactManager = new ContactManagerImpl();
-    int contactId = contactManager.addNewContact("John", "A note about John");
+  public void testRetrievingSingleContactsByName() {
+    ContactManager contactManager = new ContactManagerImpl();
+    generateTestContacts(contactManager);
 
-    Set<Contact> contactByName = contactManager.getContacts("John");
-    assertEquals("John", ((Contact) contactByName.toArray()[0]).getName());
+    contactManager.addNewContact("John", "A note about John");
+    Set<Contact> contactsByName = contactManager.getContacts("John");
+    Contact contact = (Contact) Array.get(contactsByName.toArray(), 0);
+
+    assertEquals("John", contact.getName());
+  }
+
+  private List<Integer> generateTestContacts(ContactManager contactManager) {
+    String[] names = new String[] { "Jerry", "Kelly", "Thomas", "Laura" };
+    List<Integer> contactIds = new ArrayList<Integer>();
+
+    for(int i = 0; i < names.length; i++) {
+      contactIds.add(
+        contactManager.addNewContact(names[i], "A note about " + names[i])
+      );
+    }
+
+    return contactIds;
   }
 }
