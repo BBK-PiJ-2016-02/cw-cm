f727a4c00955b82e5abd984bc7ca4a18d5d3b570
Alun Davey <hey@4lun.io>
2017-03-16 19:06:37 +0000

    Test retrieving multiple contacts at a time via IDs

--- src/test/java/test/ContactManagerTest.java
+++ src/test/java/test/ContactManagerTest.java
@@ -43,6 +43,24 @@ public class ContactManagerTest {
   }
 
   @Test
+  public void testRetrievingMultipleContactsById() {
+    ContactManager contactManager = new ContactManagerImpl();
+    generateTestContacts(contactManager);
+
+    int contactId1 = contactManager.addNewContact("John", "A note about John");
+    int contactId2 = contactManager.addNewContact("Hannah", "A note about Hannah");
+    Set<Contact> contactsById = contactManager.getContacts(contactId1, contactId2);
+
+    Contact contact1 = (Contact) Array.get(contactsById.toArray(), 0);
+    Contact contact2 = (Contact) Array.get(contactsById.toArray(), 1);
+
+    System.out.println(contact1.getName() + ", " + contact2.getName());
+
+    assertEquals("John", contact1.getName());
+    assertEquals("Hannah", contact2.getName());
+  }
+
+  @Test
   public void testRetrievingSingleContactsByName() {
     ContactManager contactManager = new ContactManagerImpl();
     generateTestContacts(contactManager);
